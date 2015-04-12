1faeab6d5ae4864420f743387037b313ee227ec7
cwhitten1 <cwhitten@ufl.edu>
2015-04-12 00:09:04 -0400

    Forgot this file in IO redirect commit

--- io_redir.c
+++ io_redir.c
@@ -1,38 +1,43 @@
 #include "io_redir.h"
 
-void redirect_in(char* fn){
+//Should be called from a forked process
+//Returns 0 if redirect successful, -1 otherwise
+int redirect_in(char* fn){
 	if(fn == NULL)
-		return;
+		return -1;
 
 	int fd = open(fn, O_RDONLY);
 
 	if(fd == -1)
-	{
-		printf("\tFile %s not readable or does not exist.\n", fn);
-		return;
-	}
+		return -1; 
+	
 
-	printf("\tFile %s found!\n", fn);
+	//printf("\tFile %s found!\n", fn);
 
 	dup2(fd, 0);
 	close(fd);
+
+	return 0;
 }
-void redirect_out(char* fn, int append){
+
+//Should be called from a forked process
+//Returns 0 if redirect successful, -1 otherwise
+int redirect_out(char* fn, int append){
 	if(fn == NULL)
-		return;
+		return -1;
 	
 	int fd;
-
-	if(append)
+	printf("%d\n",append );
+	if(append == 1)
 		fd = open(fn, O_CREAT | O_WRONLY | O_APPEND,  S_IREAD | S_IWRITE);
 	else
-		fd = open(fn, O_CREAT | O_WRONLY,  S_IREAD | S_IWRITE);
+		fd = open(fn, O_CREAT | O_WRONLY | O_TRUNC,  S_IREAD | S_IWRITE);
 
 	dup2(fd, 1);
-	//close(fd);
-
-
+	close(fd);
+	return 0;
 }
+
 void reset_in(){
 
 }
