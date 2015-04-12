00fbe635a8bae012e81624805029b5991b5b484e
cwhitten1 <cwhitten@ufl.edu>
2015-04-12 00:09:47 -0400

    Cleaned up header

--- io_redir.h
+++ io_redir.h
@@ -4,9 +4,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define redir_in 1
-#define redir_out 2
-#define no_redir 3
 int redirect_in(char* fn);		/* Redirect stdin to file fn*/
 int redirect_out(char* fn, int append);	/* Redirect stdout to file fo*/
 void reset_in();				/* Reset stdin to default*/
